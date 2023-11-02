ac5b75fa56a28625dcf8234b6d982ff938ed6e68
Adrian <adriangonzalesval@gmail.com>
2023-11-02 17:26:32 -0500

    fix: Rewritten backend signed queue logic

--- backend/common/src/main/java/io/github/_4drian3d/signedvelocity/common/queue/QueuedData.java
+++ backend/common/src/main/java/io/github/_4drian3d/signedvelocity/common/queue/QueuedData.java
@@ -1,51 +1,44 @@
 package io.github._4drian3d.signedvelocity.common.queue;
 
 import io.github._4drian3d.signedvelocity.common.PropertyHolder;
+import org.jetbrains.annotations.Nullable;
 
+import java.util.Queue;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.TimeUnit;
 
 public final class QueuedData {
-    private static final int timeout = PropertyHolder.readInt("io.github._4drian3d.signedvelocity.timeout", 140);
-    private volatile CompletableFuture<SignedResult> futureResult;
+    private static final int timeout = PropertyHolder.readInt("io.github._4drian3d.signedvelocity.timeout", 100);
+    private final Queue<SignedResult> results = new ConcurrentLinkedQueue<>();
+    private final Queue<CompletableFuture<SignedResult>> unSyncronizedQueue = new ConcurrentLinkedQueue<>();
 
-    // first
     public void complete(final SignedResult result) {
-        if (this.futureResult != null) {
-            // UnSynchronized
-            if (!this.futureResult.isDone()) {
-                this.futureResult.complete(result);
-                this.futureResult = null;
-            }
-        } else {
-            // Synchronized
-            this.futureResult = CompletableFuture.completedFuture(result);
+        this.results.add(result);
+        final var unSynchronized = unSyncronizedQueue.poll();
+        if (unSynchronized != null) {
+            unSynchronized.complete(result);
         }
     }
 
-    //second
     public CompletableFuture<SignedResult> nextResult() {
-        if (this.futureResult == null) {
-            // UnSynchronized
-            return (futureResult = new CompletableFuture<>())
-                    .completeOnTimeout(SignedResult.allowed(), timeout, TimeUnit.MILLISECONDS);
-        } else {
-            // Synchronized
-            final CompletableFuture<SignedResult> actual = this.futureResult;
-            this.futureResult = null;
-            return actual.completeOnTimeout(SignedResult.allowed(), timeout, TimeUnit.MILLISECONDS);
-        }
+        final SignedResult result = results.poll();
+        return futureFrom(result);
     }
 
-    @SuppressWarnings("ReplaceNullCheck")
     public CompletableFuture<SignedResult> nextResultWithoutAdvance() {
-        if (this.futureResult == null) {
-            // UnSynchronized
-            return (futureResult = new CompletableFuture<>())
-                    .completeOnTimeout(SignedResult.allowed(), timeout, TimeUnit.MILLISECONDS);
+        final SignedResult result = results.peek();
+        return futureFrom(result);
+    }
+
+    private CompletableFuture<SignedResult> futureFrom(final @Nullable SignedResult result) {
+        if (result == null) {
+            final CompletableFuture<SignedResult> future = new CompletableFuture<>();
+            future.completeOnTimeout(SignedResult.allowed(), timeout, TimeUnit.MILLISECONDS);
+            unSyncronizedQueue.add(future);
+            return future;
         } else {
-            // Synchronized
-            return this.futureResult.completeOnTimeout(SignedResult.allowed(), timeout, TimeUnit.MILLISECONDS);
+            return CompletableFuture.completedFuture(result);
         }
     }
 }
