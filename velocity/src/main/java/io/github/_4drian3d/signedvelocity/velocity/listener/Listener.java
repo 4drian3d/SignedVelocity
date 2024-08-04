86199147d4366126713ffb9b9a5693bb15f97e66
Adrian <adriangonzalesval@gmail.com>
2024-08-04 09:15:57 -0500

    fix: Fixed some plugin messaging issue

--- velocity/src/main/java/io/github/_4drian3d/signedvelocity/velocity/listener/Listener.java
+++ velocity/src/main/java/io/github/_4drian3d/signedvelocity/velocity/listener/Listener.java
@@ -3,13 +3,14 @@ package io.github._4drian3d.signedvelocity.velocity.listener;
 import com.google.inject.Injector;
 import com.velocitypowered.api.event.AwaitingEventExecutor;
 
-public sealed interface Listener<E> extends AwaitingEventExecutor<E> permits PlayerChatListener, PlayerCommandListener {
+public sealed interface Listener<E> extends AwaitingEventExecutor<E> permits PlayerChatListener, PlayerCommandListener, PluginMessageListener {
     void register();
 
     static void register(final Injector injector) {
         final Listener<?>[] listeners = {
                 injector.getInstance(PlayerChatListener.class),
-                injector.getInstance(PlayerCommandListener.class)
+                injector.getInstance(PlayerCommandListener.class),
+                injector.getInstance(PluginMessageListener.class)
         };
         for (final Listener<?> listener : listeners) {
             listener.register();
---
+++ velocity/src/main/java/io/github/_4drian3d/signedvelocity/velocity/listener/PluginMessageListener.java
@@ -0,0 +1,35 @@
+package io.github._4drian3d.signedvelocity.velocity.listener;
+
+import com.google.inject.Inject;
+import com.velocitypowered.api.event.EventManager;
+import com.velocitypowered.api.event.EventTask;
+import com.velocitypowered.api.event.connection.PluginMessageEvent;
+import com.velocitypowered.api.proxy.Player;
+import io.github._4drian3d.signedvelocity.velocity.SignedVelocity;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+
+import java.util.Objects;
+
+public final class PluginMessageListener implements Listener<PluginMessageEvent> {
+  @Inject
+  private EventManager eventManager;
+  @Inject
+  private SignedVelocity plugin;
+
+
+  @Override
+  public void register() {
+    eventManager.register(plugin, PluginMessageEvent.class, this);
+  }
+
+  @Override
+  public EventTask executeAsync(PluginMessageEvent event) {
+    return EventTask.async(() -> {
+      if (Objects.equals(event.getIdentifier(), SignedVelocity.SIGNEDVELOCITY_CHANNEL)
+              && event.getSource() instanceof Player player) {
+       player.disconnect(Component.translatable("velocity.error.internal-server-connection-error", NamedTextColor.RED));
+      }
+    });
+  }
+}
