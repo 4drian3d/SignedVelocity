b4185b679d208cc99ec9078fea29e3b8dc0a026a
Adrian <adriangonzalesval@gmail.com>
2024-08-05 14:25:10 -0500

    feat: Re-added secure chat popup support

--- velocity/src/main/java/io/github/_4drian3d/signedvelocity/velocity/packet/PacketEventsAdapter.java
+++ velocity/src/main/java/io/github/_4drian3d/signedvelocity/velocity/packet/PacketEventsAdapter.java
@@ -5,6 +5,7 @@ import com.github.retrooper.packetevents.event.PacketListener;
 import com.github.retrooper.packetevents.event.PacketListenerPriority;
 import com.github.retrooper.packetevents.event.PacketSendEvent;
 import com.github.retrooper.packetevents.protocol.packettype.PacketType;
+import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerJoinGame;
 import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerServerData;
 
 final class PacketEventsAdapter implements PacketAdapter {
@@ -19,12 +20,13 @@ final class PacketEventsAdapter implements PacketAdapter {
   private static final class DataListener implements PacketListener {
     @Override
     public void onPacketSend(final PacketSendEvent event) {
-      if (event.getPacketType() != PacketType.Play.Server.SERVER_DATA) {
-        return;
-      }
-      final WrapperPlayServerServerData packet = new WrapperPlayServerServerData(event);
-      if (!packet.isEnforceSecureChat()) {
+      if (event.getPacketType() == PacketType.Play.Server.SERVER_DATA) {
+        final WrapperPlayServerServerData packet = new WrapperPlayServerServerData(event);
         packet.setEnforceSecureChat(true);
+      }
+      if (event.getPacketType() == PacketType.Play.Server.JOIN_GAME) {
+        final WrapperPlayServerJoinGame packet = new WrapperPlayServerJoinGame(event);
+        packet.setEnforcesSecureChat(true);
       }
     }
   }
--- velocity/src/main/java/io/github/_4drian3d/signedvelocity/velocity/packet/VPacketEventsAdapter.java
+++ velocity/src/main/java/io/github/_4drian3d/signedvelocity/velocity/packet/VPacketEventsAdapter.java
@@ -2,24 +2,12 @@ package io.github._4drian3d.signedvelocity.velocity.packet;
 
 import com.google.inject.Inject;
 import com.velocitypowered.api.event.EventManager;
+import com.velocitypowered.proxy.protocol.packet.JoinGamePacket;
 import com.velocitypowered.proxy.protocol.packet.ServerDataPacket;
 import io.github._4drian3d.signedvelocity.velocity.SignedVelocity;
 import io.github._4drian3d.vpacketevents.api.event.PacketSendEvent;
 
-import java.lang.invoke.MethodHandle;
-import java.lang.invoke.MethodHandles;
-
 final class VPacketEventsAdapter implements PacketAdapter {
-  private static final MethodHandle ENFORCED_SETTER;
-
-  static {
-    try {
-      final MethodHandles.Lookup lookup = MethodHandles.privateLookupIn(ServerDataPacket.class, MethodHandles.lookup());
-      ENFORCED_SETTER = lookup.findSetter(ServerDataPacket.class, "secureChatEnforced", Boolean.TYPE);
-    } catch (Exception e) {
-      throw new RuntimeException(e);
-    }
-  }
 
   @Inject
   private EventManager eventManager;
@@ -29,16 +17,11 @@ final class VPacketEventsAdapter implements PacketAdapter {
   @Override
   public void register() {
     eventManager.register(plugin, PacketSendEvent.class, event -> {
-      if (!(event.getPacket() instanceof final ServerDataPacket serverData)) {
-        return;
+      if (event.getPacket() instanceof final ServerDataPacket serverData) {
+        serverData.setSecureChatEnforced(true);
       }
-      if (serverData.isSecureChatEnforced()) {
-        return;
-      }
-      try {
-        ENFORCED_SETTER.invoke(serverData, true);
-      } catch (Throwable e) {
-        throw new RuntimeException(e);
+      if (event.getPacket() instanceof final JoinGamePacket joinPacket) {
+        joinPacket.setEnforcesSecureChat(true);
       }
     });
   }
