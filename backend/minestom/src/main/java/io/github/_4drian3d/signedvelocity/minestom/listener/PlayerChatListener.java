826836d6e2052ab58db578ae30dd40a593d08d5e
Christian Bergschneider <christian.bergschneider@gmx.de>
2025-04-09 09:52:44 +0200

    feat: enable minestom 1.21.5 and lower compatibility

--- backend/minestom/src/main/java/io/github/_4drian3d/signedvelocity/minestom/listener/PlayerChatListener.java
+++ backend/minestom/src/main/java/io/github/_4drian3d/signedvelocity/minestom/listener/PlayerChatListener.java
@@ -1,12 +1,34 @@
 package io.github._4drian3d.signedvelocity.minestom.listener;
 
 import io.github._4drian3d.signedvelocity.minestom.SignedVelocity;
+import net.minestom.server.MinecraftServer;
 import net.minestom.server.entity.Player;
 import net.minestom.server.event.player.PlayerChatEvent;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.function.Consumer;
 
 public final class PlayerChatListener implements Consumer<PlayerChatEvent> {
+    private static final Field rawMessage;
+    private static final Field formattedMessage;
+    private static final Method buildDefaultChatMessage;
+
+    static {
+        try {
+            rawMessage = PlayerChatEvent.class.getDeclaredField("rawMessage");
+            rawMessage.setAccessible(true);
+
+            formattedMessage = PlayerChatEvent.class.getDeclaredField("formattedMessage");
+            formattedMessage.setAccessible(true);
+
+            buildDefaultChatMessage = PlayerChatEvent.class.getDeclaredMethod("buildDefaultChatMessage");
+            buildDefaultChatMessage.setAccessible(true);
+        } catch (NoSuchFieldException | NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     @Override
     public void accept(final PlayerChatEvent event) {
@@ -19,7 +41,14 @@ public final class PlayerChatListener implements Consumer<PlayerChatEvent> {
                     } else {
                         final String modifiedChat = result.toModify();
                         if (modifiedChat != null) {
-                            event.setMessage(modifiedChat);
+                            // set new raw message
+                            try {
+                                rawMessage.set(event, modifiedChat);
+                                Object newFormatted = buildDefaultChatMessage.invoke(event);
+                                formattedMessage.set(event, newFormatted);
+                            } catch (IllegalAccessException | InvocationTargetException e) {
+                                MinecraftServer.getExceptionManager().handleException(e);
+                            }
                         }
                     }
                 }).join();
