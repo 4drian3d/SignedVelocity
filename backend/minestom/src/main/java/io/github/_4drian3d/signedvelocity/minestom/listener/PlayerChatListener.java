e4c3b882fd2cfa2699f88c4a43ae4bd796d74830
Adrian Gonzales <adriangonzalesval@gmail.com>
2025-09-09 00:17:35 -0500

    fix: Updated Minestom Module
    resolves #144
    
    Co-authored-by: Christian Bergschneider <christian.bergschneider@gmx.de>

--- backend/minestom/src/main/java/io/github/_4drian3d/signedvelocity/minestom/listener/PlayerChatListener.java
+++ backend/minestom/src/main/java/io/github/_4drian3d/signedvelocity/minestom/listener/PlayerChatListener.java
@@ -1,12 +1,32 @@
 package io.github._4drian3d.signedvelocity.minestom.listener;
 
 import io.github._4drian3d.signedvelocity.minestom.SignedVelocity;
+import net.kyori.adventure.text.Component;
+import net.minestom.server.MinecraftServer;
 import net.minestom.server.entity.Player;
 import net.minestom.server.event.player.PlayerChatEvent;
 
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
+import java.lang.invoke.VarHandle;
 import java.util.function.Consumer;
 
 public final class PlayerChatListener implements Consumer<PlayerChatEvent> {
+    private static final VarHandle rawMessage;
+    private static final MethodHandle buildDefaultChatMessage;
+
+    static {
+        try {
+            final MethodHandles.Lookup lookup = MethodHandles.privateLookupIn(PlayerChatEvent.class, MethodHandles.lookup());
+            rawMessage = lookup.findVarHandle(PlayerChatEvent.class, "rawMessage", String.class);
+
+            final MethodType chatMessageMethodType = MethodType.methodType(Component.class);
+            buildDefaultChatMessage = lookup.findVirtual(PlayerChatEvent.class, "buildDefaultChatMessage", chatMessageMethodType);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     @Override
     public void accept(final PlayerChatEvent event) {
@@ -19,7 +39,12 @@ public final class PlayerChatListener implements Consumer<PlayerChatEvent> {
                     } else {
                         final String modifiedChat = result.toModify();
                         if (modifiedChat != null) {
-                            event.setMessage(modifiedChat);
+                            try {
+                                rawMessage.set(event, modifiedChat);
+                                event.setFormattedMessage((Component) buildDefaultChatMessage.invoke(event));
+                            } catch (Throwable e) {
+                                MinecraftServer.getExceptionManager().handleException(e);
+                            }
                         }
                     }
                 }).join();
