93ef2bb2eb9579cd8db6ee10386941c16c1926a4
Adrian Gonzales <adriangonzalesval@gmail.com>
2025-09-09 00:26:37 -0500

    publish: Update Minestom module publishing

--- backend/minestom/build.gradle.kts
+++ backend/minestom/build.gradle.kts
@@ -1,12 +1,8 @@
+import java.time.LocalDate
+
 plugins {
     `java-library`
-    `maven-publish`
-    signing
-}
-
-java {
-    withSourcesJar()
-    withJavadocJar()
+    id("com.vanniktech.maven.publish") version "0.34.0"
 }
 
 dependencies {
@@ -16,66 +12,41 @@ dependencies {
     api(projects.signedvelocityBackendCommon)
 }
 
-// Publish to Maven Central
-//publishing {
-//    publications {
-//        create<MavenPublication>("maven") {
-//            repositories {
-//                maven {
-//                    credentials {
-//                        username = property("sonatypeTokenUsername")?.toString() ?: ""
-//                        password = property("sonatypeTokenPassword")?.toString() ?: ""
-//                    }
-//                    val central = "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
-//                    val snapshots = "https://s01.oss.sonatype.org/content/repositories/snapshots/"
-//                    if (project.version.toString().endsWith("SNAPSHOT")) {
-//                        name = "SonatypeSnapshots"
-//                        setUrl(snapshots)
-//                    } else {
-//                        name = "OSSRH"
-//                        setUrl(central)
-//                    }
-//                }
-//            }
-//            from(components["java"])
-//            pom {
-//                url.set("https://github.com/4drian3d/SignedVelocity")
-//                licenses {
-//                    license {
-//                        name.set("GNU General Public License version 3 or later")
-//                        url.set("https://opensource.org/licenses/GPL-3.0")
-//                    }
-//                }
-//                scm {
-//                    connection.set("scm:git:https://github.com/4drian3d/SignedVelocity.git")
-//                    developerConnection.set("scm:git:ssh://git@github.com/4drian3d/SignedVelocity.git")
-//                    url.set("https://github.com/4drian3d/SignedVelocity")
-//                }
-//                developers {
-//                    developer {
-//                        id.set("4drian3d")
-//                        name.set("Adrian Gonzales")
-//                        email.set("adriangonzalesval@gmail.com")
-//                    }
-//                }
-//                issueManagement {
-//                    name.set("GitHub")
-//                    url.set("https://github.com/4drian3d/SignedVelocity/issues")
-//                }
-//                ciManagement {
-//                    name.set("GitHub Actions")
-//                    url.set("https://github.com/4drian3d/SignedVelocity/actions")
-//                }
-//                name.set(project.name)
-//                description.set(project.description)
-//                url.set("https://github.com/4drian3d/SignedVelocity")
-//            }
-//            artifactId = "signedvelocity-minestom"
-//        }
-//    }
-//}
-//signing {
-//    useGpgCmd()
-//    sign(configurations.archives.get())
-//    sign(publishing.publications["maven"])
-//}
+mavenPublishing {
+    publishToMavenCentral()
+    signAllPublications()
+    coordinates(project.group as String, "signedvelocity-minestom", project.version as String)
+
+    pom {
+        name.set(project.name)
+        description.set(project.description)
+        inceptionYear.set(LocalDate.now().year.toString())
+        url.set("https://github.com/4drian3d/SignedVelocity")
+        licenses {
+            license {
+                name.set("GNU General Public License version 3 or later")
+                url.set("https://opensource.org/licenses/GPL-3.0")
+            }
+        }
+        developers {
+            developer {
+                id.set("4drian3d")
+                name.set("Adrian Gonzales")
+                email.set("adriangonzalesval@gmail.com")
+            }
+        }
+        scm {
+            connection.set("scm:git:https://github.com/4drian3d/SignedVelocity.git")
+            developerConnection.set("scm:git:ssh://git@github.com/4drian3d/SignedVelocity.git")
+            url.set("https://github.com/4drian3d/SignedVelocity")
+        }
+        ciManagement {
+            name.set("GitHub Actions")
+            url.set("https://github.com/4drian3d/SignedVelocity")
+        }
+        issueManagement {
+            name.set("GitHub")
+            url.set("https://github.com/4drian3d/SignedVelocity/issues")
+        }
+    }
+}
--- backend/minestom/src/main/java/io/github/_4drian3d/signedvelocity/minestom/listener/PlayerChatListener.java
+++ backend/minestom/src/main/java/io/github/_4drian3d/signedvelocity/minestom/listener/PlayerChatListener.java
@@ -6,23 +6,18 @@ import net.minestom.server.MinecraftServer;
 import net.minestom.server.entity.Player;
 import net.minestom.server.event.player.PlayerChatEvent;
 
-import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
-import java.lang.invoke.MethodType;
 import java.lang.invoke.VarHandle;
 import java.util.function.Consumer;
 
 public final class PlayerChatListener implements Consumer<PlayerChatEvent> {
     private static final VarHandle rawMessage;
-    private static final MethodHandle buildDefaultChatMessage;
 
     static {
         try {
-            final MethodHandles.Lookup lookup = MethodHandles.privateLookupIn(PlayerChatEvent.class, MethodHandles.lookup());
+            final MethodHandles.Lookup lookup = MethodHandles.privateLookupIn(
+                    PlayerChatEvent.class, MethodHandles.lookup());
             rawMessage = lookup.findVarHandle(PlayerChatEvent.class, "rawMessage", String.class);
-
-            final MethodType chatMessageMethodType = MethodType.methodType(Component.class);
-            buildDefaultChatMessage = lookup.findVirtual(PlayerChatEvent.class, "buildDefaultChatMessage", chatMessageMethodType);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
@@ -41,7 +36,12 @@ public final class PlayerChatListener implements Consumer<PlayerChatEvent> {
                         if (modifiedChat != null) {
                             try {
                                 rawMessage.set(event, modifiedChat);
-                                event.setFormattedMessage((Component) buildDefaultChatMessage.invoke(event));
+                                event.setFormattedMessage(Component.translatable("chat.type.text")
+                                        .arguments(
+                                                Component.text(player.getUsername())
+                                                        .insertion(player.getUsername())
+                                                        .hoverEvent(player),
+                                                Component.text(modifiedChat)));
                             } catch (Throwable e) {
                                 MinecraftServer.getExceptionManager().handleException(e);
                             }
