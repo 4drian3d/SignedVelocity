4342acc24988752c14e528879a414e81002e08ee
Adrian Gonzales <adriangonzalesval@gmail.com>
2025-10-04 21:31:25 -0500

    fix: Publish shared module

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,3 +1,49 @@
+import java.time.LocalDate
+
+plugins {
+    `java-library`
+    id("com.vanniktech.maven.publish") version "0.34.0"
+}
+
 dependencies {
     compileOnly(libs.slf4j)
+}
+
+mavenPublishing {
+    publishToMavenCentral()
+    signAllPublications()
+    coordinates(project.group as String, "signedvelocity-shared", project.version as String)
+
+    pom {
+        name.set(project.name)
+        description.set(project.description)
+        inceptionYear.set(LocalDate.now().year.toString())
+        url.set("https://github.com/4drian3d/SignedVelocity")
+        licenses {
+            license {
+                name.set("GNU General Public License version 3 or later")
+                url.set("https://opensource.org/licenses/GPL-3.0")
+            }
+        }
+        developers {
+            developer {
+                id.set("4drian3d")
+                name.set("Adrian Gonzales")
+                email.set("adriangonzalesval@gmail.com")
+            }
+        }
+        scm {
+            connection.set("scm:git:https://github.com/4drian3d/SignedVelocity.git")
+            developerConnection.set("scm:git:ssh://git@github.com/4drian3d/SignedVelocity.git")
+            url.set("https://github.com/4drian3d/SignedVelocity")
+        }
+        ciManagement {
+            name.set("GitHub Actions")
+            url.set("https://github.com/4drian3d/SignedVelocity")
+        }
+        issueManagement {
+            name.set("GitHub")
+            url.set("https://github.com/4drian3d/SignedVelocity/issues")
+        }
+    }
 }
